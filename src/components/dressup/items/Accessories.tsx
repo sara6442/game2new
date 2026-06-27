5c40fd6d94f0502f4ea8ebd466a665e6c8be14cd
sara6442 <sara.ii664422@gmail.com>
2026-06-27 18:41:58 +0300

    Delete src/components/dressup/items/Accessories.tsx

--- src/components/dressup/items/Accessories.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-export interface AccessoryProps { color?: string; trim?: string }
-export interface AccessoryItem { id: string; name: string; Component: React.FC<AccessoryProps> }
-export const ACCESSORY_ITEMS: AccessoryItem[] = []
-export default ACCESSORY_ITEMS
